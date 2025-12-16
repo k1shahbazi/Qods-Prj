82748f9b4af936d67d0b65005c9304ad39b2d90e
k1shahbazi <k.1kingplay@gmail.com>
2025-12-16 22:22:30 +0330

    updated

---
+++ matrix/module_confusion_matrix.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+def plot_conf_matrix(y_true, y_pred, class_names):
+    cm = confusion_matrix(y_true, y_pred)
+
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(
+        cm,
+        annot=True,
+        fmt="d",
+        cmap="Blues",
+        xticklabels=class_names,
+        yticklabels=class_names
+    )
+    plt.xlabel("Predicted")
+    plt.ylabel("True")
+    plt.title("Confusion Matrix")
+    plt.show()
